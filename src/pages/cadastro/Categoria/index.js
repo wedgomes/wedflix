ad35d4cecd053b520bfdf8e0c80f83639037d046
wedgomes <wedson_mxt@hotmail.com>
2020-10-13 22:05:34 -0300

    mudança na alteração da categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria(){
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+
     return(
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {nomeDaCategoria} </h1>
 
             <form>
                 <label>
                     Nome da Categoria:
-                    <input type="text"/>
+                    <input 
+                        type="text"
+                        value={nomeDaCategoria}
+                        onChange={function funcaoHandlerQueOErroPediu(infosDoEvento) {
+                            console.log('[nomeDaCategoria]', nomeDaCategoria);
+                            console.log('[infosDoEvento.target.value]', infosDoEvento.target.value);
+                            setNomeDaCategoria(infosDoEvento.target.value);
+                        }}
+                    />   
                 </label>
 
                 <button>
